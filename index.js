f4d3b13e5a23a79920bb39dafe409a84978360a8
Abyeon <wpseals@gmail.com>
2021-06-25 00:02:28 -0400

    Add basic bot boiler

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login(token);
