78a16c0161f54bffe8c5f4c387c68a41e9ab8488
Abyeon <wpseals@gmail.com>
2021-06-25 04:04:42 -0400

    Add new event handlers

---
+++ events/message.js
@@ -0,0 +1,72 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('../config.json');
+
+module.exports = {
+    name: 'message',
+    execute(message, client) {
+        //console.log(`${message.guild.name}/#${message.channel.name}/@${message.member.user.username}#${message.member.user.discriminator}: ${message.content}`); // TODO: Make logger
+    
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+        if (!command) return; // If the command does not exist, return.
+
+        // Check if in DMs
+        if ((command.guildOnly || command.permissions) && message.channel.type === 'dm') {
+            return message.reply('Cant execute this command inside DMs!');
+        }
+
+        // Check if user has permission to use command
+        if (command.permissions) {
+            const authorPerms = message.channel.permissionsFor(message.author);
+            if (!authorPerms || !authorPerms.has(command.permissions)) {
+                return;
+            }
+        }
+
+        // Check if there are args if required
+        if (command.args && !args.length) {
+            let reply = `No arguments provided!`;
+
+            if (command.usage) {
+                reply += `\nUsage: \`${prefix}${command.name} ${command.usage}`;
+            }
+
+            return message.channel.send(reply);
+        }
+
+        // Make sure user cant execute a command within cooldown period.
+        const { cooldowns } = client;
+
+        if (!cooldowns.has(command.name)) {
+            cooldowns.set(command.name, new Discord.Collection());
+        }
+
+        const now = Date.now();
+        const timestamps = cooldowns.get(command.name);
+        const cooldownAmount = (command.cooldown || 3) * 1000;
+
+        if (timestamps.has(message.author.id)) {
+            const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+            if (now < expirationTime) {
+                const timeLeft = (expirationTime - now) / 1000;
+                return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+            }
+        }
+
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+        try {
+            command.execute(message, args);
+        } catch (err) {
+            console.error(err);
+            message.reply('there was an error trying to execute that command.');
+        }
+    }
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.cooldowns = new Discord.Collection();
 
+/* Register Commands */
 const commandFolders = fs.readdirSync('./commands');
 
 for (const folder of commandFolders) {
@@ -16,75 +17,17 @@ for (const folder of commandFolders) {
     }
 }
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
+/* Register Events */
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-client.on('message', message => {
-    //console.log(`${message.guild.name}/#${message.channel.name}/@${message.member.user.username}#${message.member.user.discriminator}: ${message.content}`); // TODO: Make logger
-    
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-    if (!command) return; // If the command does not exist, return.
-
-    // Check if in DMs
-    if ((command.guildOnly || command.permissions) && message.channel.type === 'dm') {
-        return message.reply('Cant execute this command inside DMs!');
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
     }
+}
 
-    // Check if user has permission to use command
-    if (command.permissions) {
-        const authorPerms = message.channel.permissionsFor(message.author);
-        if (!authorPerms || !authorPerms.has(command.permissions)) {
-            return;
-        }
-    }
-
-    // Check if there are args if required
-    if (command.args && !args.length) {
-        let reply = `No arguments provided!`;
-
-        if (command.usage) {
-            reply += `\nUsage: \`${prefix}${command.name} ${command.usage}`;
-        }
-
-        return message.channel.send(reply);
-    }
-
-    // Make sure user cant execute a command within cooldown period.
-    const { cooldowns } = client;
-
-    if (!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Discord.Collection());
-    }
-
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
-
-    if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-        if (now < expirationTime) {
-            const timeLeft = (expirationTime - now) / 1000;
-            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-        }
-    }
-
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-    try {
-        command.execute(message, args);
-    } catch (err) {
-        console.error(err);
-        message.reply('there was an error trying to execute that command.');
-    }
-});
-
+/* Login */
 client.login(token);
