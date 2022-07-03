7ee30de0859e7c0ee89e131a043dea530b062494
Abyeon <wpseals@gmail.com>
2022-07-03 16:36:43 -0400

    Remove redundant code and add total value to embed

--- commands/fun/freegames.js
+++ commands/fun/freegames.js
@@ -9,7 +9,7 @@ module.exports = {
     name: 'freegames',
     description: 'Responds with a list of current free games for the chosen platform',
     acceptedArguments: 'pc, steam, epic-games-store, ubisoft, gog, itchio, ps4, ps5, xbox-one, xbox-series-xs, switch, android, ios, vr, battlenet, origin, drm-free, xbox-360',
-    aliases: ['free'],
+    aliases: ['free', 'games'],
     usage: '(platform)',
     cooldown: 2,
     
@@ -44,31 +44,32 @@ module.exports = {
             .setColor(hexColor)
             .setTimestamp()
 
-        let url = `https://www.gamerpower.com/api/filter?platform=${chosenPlatform}&type=game&sort-by=value`;
+        let url = `https://www.gamerpower.com/api/filter?platform=${chosenPlatform}&type=game&sort-by=popularity`;
 
         fetch(url, settings)
             .then(res => res.json())
             .then((json) => {
-                for (var i = 0; i < 20 && i < json.length; i++) {
-                    // Check if returned json includes a platform from our chosen platforms.
-                    let gpPlatforms = json[i].platforms.split(', ');
-                    let addToList = gpPlatforms.map(gpPlatform => {
-                        if (matchingPlatforms.includes(gpPlatform)) {
-                            return true;
-                        }
-                    })
+                let value = 0;
 
-                    // Add it to the returned embed.
-                    if (addToList) {
-                        list += `\n${json[i].worth} [${json[i].title}](${json[i].open_giveaway})`;
+                for (var i = 0; i < 20 && i < json.length; i++) {
+                    // Convert json string to float value
+                    parsedWorth = parseFloat(json[i].worth.substring(1))
+
+                    // Check to see if float value is usable
+                    if (!isNaN(parsedWorth)) {
+                        // If so, add it the total value
+                        value += parsedWorth;
                     }
+
+                    // Add game to embed
+                    list += `\n${json[i].worth} [${json[i].title}](${json[i].open_giveaway})`;
                 }
 
                 if (json.length == undefined) {
                     embed.setTitle("ERROR!");
                     embed.setDescription(json.status_message);
                 } else {
-                    embed.setTitle(`Top ${(20 > json.length) ? json.length : 20} Current Free Games For ${matchingPlatforms.join(' ').toUpperCase()}`)
+                    embed.setTitle(`Top ${(20 > json.length) ? json.length : 20} Free Games For ${matchingPlatforms.join(' ').toUpperCase()} Worth $${value.toFixed(2)}!`)
                     embed.setDescription(list);
                 }
                 
